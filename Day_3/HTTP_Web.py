694c4ef605c50ed2e81bb686b03e3dd2c687ed19
Mustafa Ngure <mustafangure@gmail.com>
2017-04-06 22:24:14 +0300

    consume footballteam information from public API

--- Day_3/HTTP_Web.py
+++ Day_3/HTTP_Web.py
@@ -3,8 +3,8 @@ import json
 
 r = http.client.HTTPConnection('api.football-data.org')
 headers = {'Content-type':'application/json',
-             'Accept':'application/json'}
-r.request('GET', '/v1/competitions', None, headers )
+             'Accept':'application/json','X-Response-Control': 'minified'}
+r.request('GET', '/v1/teams/66/', None, headers )
 result = json.loads(r.getresponse().read().decode())
 
 print (result)
